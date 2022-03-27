49a145d2093613710071ef1dbd3251b99ef93049
Angga Arya Saputra <anggaarya53@gmail.com>
2022-03-27 14:03:14 +0700

    bisa auth

--- lib/tdlib.dart
+++ lib/tdlib.dart
@@ -1,3 +1,14 @@
 library tdlib;
 
+import 'package:flutter/services.dart';
+
 export 'package:tdlib/src/tdclient/td_client.dart';
+
+class TdLib {
+  static const MethodChannel _channel = MethodChannel('tdlib');
+
+  static Future<String?> get platformVersion async {
+    final String? version = await _channel.invokeMethod('getPlatformVersion');
+    return version;
+  }
+}
